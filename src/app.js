97b1a290512cc222d79c5c98f2a217e83b162f81
dylansanjaya <dylanhikmasanjaya@gmail.com>
2024-06-08 14:43:27 +0700

    init 2

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const config = require("./config/config");
-
-const app = express();
-
-app.use(express.json());
-
-module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  cors = require("cors"),
+  router = require("./src/routers"),
+  bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+app.use(express.json({ strict: false }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/", router);
+
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
+});
+
+module.exports = app;
